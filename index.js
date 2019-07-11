21df947be0ba45bf9806fb04a2a4219146249d58
Atte Laakso <laakso95@gmail.com>
2019-07-11 09:18:52 +0300

    Exercise 3.1

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+  {
+    "id": 1,
+    "name": "Atte Gates",
+    "number": "040-123456",
+  },
+  {
+    "id": 2,
+    "name": "Bill Gates",
+    "number": "123123123",
+  },
+  {
+    "id": 3,
+    "name": "Gill Bates",
+    "number": "666",
+  },
+  {
+    "name": "testi",
+    "number": "001",
+    "id": 4
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Running on port ${port}`)
