ebd2021375f0d17e2fd21c9c8c77eaa385a25704
Atte Laakso <laakso95@gmail.com>
2019-07-12 08:57:38 +0300

    build ui

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bc04288fb09ecaaf13366e8a1a854f1.js"
+  "/precache-manifest.c3a50abad9eb999bc0b98ac8ce9b7c08.js"
 );
 
 self.addEventListener('message', (event) => {
