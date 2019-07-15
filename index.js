f438f6ea492f0191b3d6770cf021ff793eb7a37c
Atte Laakso <laakso95@gmail.com>
2019-07-15 08:55:06 +0300

    Exercise 3.17

--- index.js
+++ index.js
@@ -82,6 +82,22 @@ app.post('/api/persons', (req, res) => {
 
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
